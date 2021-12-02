86dcab8ff28bf076ec0723f87eac7bc442a0d00e
rajib miah <ahmedrajib942@gmail.com>
2021-12-02 23:50:11 +0600

    used signals for creating auto authtoken for new user

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 from django.db import models
 
 # Create your models here.
@@ -5,3 +9,11 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     roll = models.IntegerField()
     city = models.CharField(max_length=100)
+
+
+
+# this signal creates auth token for users
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
